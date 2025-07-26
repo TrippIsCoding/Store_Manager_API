709f9f5358c26512de4c271b603112993d431a80
Sean <trippiscoding@gmail.com>
2025-07-25 21:05:47 -0400

    adding requirments.txt

--- Manager_API/manager_crud.py
+++ Manager_API/manager_crud.py
@@ -12,7 +12,7 @@ async def view_inventory(db: session = Depends(get_db)):
     return [{'id': item.id, 'name': item.name, 'price': item.price, 'in_stock': item.in_stock} for item in inventory]
 
 @manager_router.post('/add')
-async def add_to_inventory(new_item: ItemModel = Depends(), db: session = Depends(get_db)):
+async def add_to_inventory(new_item: ItemModel, db: session = Depends(get_db)):
     add_new_item = Item(
         name=new_item.name,
         price=new_item.price,
@@ -26,7 +26,7 @@ async def add_to_inventory(new_item: ItemModel = Depends(), db: session = Depend
     return {'message': 'The item was added to the inventory.'}
 
 @manager_router.put('/update/{id}')
-async def update_item(id: int, updated_item: UpdateItemModel = Depends(), db: session = Depends(get_db)):
+async def update_item(id: int, updated_item: UpdateItemModel, db: session = Depends(get_db)):
     item = db.query(Item).filter(Item.id==id).first()
 
     if not item:
